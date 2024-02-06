ad914c28b48ad25caa5db7eef6425b2ba9f4ed52
JAGGANRAJ <njwebdesigning@gmail.com>
2024-02-06 14:06:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           }}
         >
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            <span className='sm:block hidden'> NJWEBDESIGNING</span>
+            NJWEBDESIGNING
           </p>
         </Link>
 
